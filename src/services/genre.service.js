6f4b087d6781f8ce579445f29e6a54805c901c84
Itacir Gabral <itacirgabral@gmail.com>
2023-06-10 19:19:20 -0400

    genre details

--- src/services/genre.service.js
+++ src/services/genre.service.js
@@ -75,15 +75,33 @@ export default class GenreService {
    * @param {string} name                     The name of the genre
    * @returns {Promise<Record<string, any>>}  The genre information
    */
-  // tag::find[]
   async find(name) {
-    // TODO: Open a new session
-    // TODO: Get Genre information from the database
-    // TODO: Throw a 404 Error if the genre is not found
-    // TODO: Close the session
+    const session = this.driver.session()
 
-    return genres.find(genre => genre.name === name)
+    const res = await session.executeRead(tx => tx.run(`
+      MATCH (g:Genre {name: $name})<-[:IN_GENRE]-(m:Movie)
+      WHERE m.imdbRating IS NOT NULL
+      AND m.poster IS NOT NULL
+      AND g.name <> '(no genres listed)'
+      WITH g, m
+      ORDER BY m.imdbRating DESC
+      WITH g, head(collect(m)) AS movie
+      RETURN g {
+        link: '/genres/'+ g.name,
+        .name,
+        movies: count { (g)<-[:IN_GENRE]-() },
+        poster: movie.poster
+      } AS genre
+    `, { name }))
+    await session.close()
+
+    if ( res.records.length === 0 ) {
+      throw new NotFoundError(`Could not find a genre with the name '${name}'`)
+    }
+
+    const [ row ] = res.records
+
+    return toNativeTypes(row.get('genre'))
   }
-  // end::find[]
 
 }
